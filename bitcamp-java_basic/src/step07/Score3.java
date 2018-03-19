60c630546f9c5b37abc1bef05362551d25cd1224
ParkHyunGi <phg9765@gmail.com>
2018-03-19 11:27:35 +0900

    .

--- bitcamp-java_basic/src/step07/Score3.java
+++ bitcamp-java_basic/src/step07/Score3.java
@@ -9,7 +9,7 @@ public class Score3 {
     int sum;
     float avg;
 
-    // 다음 연산자는 계산을 수행할 때마다 인슨턴스의 주소를 파라미터로 받아야 한다.
+    // 다음 연산자는 계산을 수행할 때마다 인슨턴스의 주소를 파라미터로 받아야 한다. 
     public static void calculate(Score3 score) {
         score.sum = score.kor + score.eng + score.math;
         score.avg = score.sum / 3f;
